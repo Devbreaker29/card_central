8ab00c70ccabf5248483f9df920de8415710e492
Devbreaker29 <hashan.pussegoda@students.mq.edu.au>
2021-02-18 10:47:28 +1100

    create unit tst and feature -listing requires title

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "requires a title" do
+      listing = Listing.new(
+        description: "cool card"
+        price: 500,
+        sold: false,
+        user: new_user
+
+      )
+
+      expect(invalid_listing.valid?).to be(false)
 end
