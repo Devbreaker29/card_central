656ac24659e55d3b712f03e2d698ebc0f7ccd1c4
Devbreaker29 <hashan.pussegoda@students.mq.edu.au>
2021-02-18 11:10:56 +1100

    done

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
-
 RSpec.describe Listing, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it "requires a title" do
-      listing = Listing.new(
-        description: "cool card"
-        price: 500,
-        sold: false,
-        user: new_user
+    new_user = User.new(
+      name: "Bob Marley",
+      email: "email@address.com",
+      password: "Password1"
+    )
 
-      )
+    new_user.save!
 
-      expect(invalid_listing.valid?).to be(false)
+    invalid_listing = Listing.new(
+      description: "cool card",
+      price: 500,
+      sold: false,
+      user: new_user
+    )
+
+    expect(invalid_listing.valid?).to be(false)
+  end
 end
